7ed101d9361158940bf00d920c17fe3a1c4100b2
Kadirmyatova Anastasiya <kadyrmatovaanastasia12@gmail.com>
2026-04-23 08:43:59 +0300

    исправления

--- src/test/java/BuyTest.java
+++ src/test/java/BuyTest.java
@@ -2,15 +2,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.util.HashMap;
 
 public class BuyTest {
 
     @Test
-    public void buyTest() {
+    public void vreifyBuy() {
         ChromeOptions options = new ChromeOptions();
         HashMap<String, Object> chromePrefs = new HashMap<>();
         chromePrefs.put("credentials_enable_service", false);
@@ -23,20 +23,17 @@ public class BuyTest {
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
 
+        SoftAssert softAssert = new SoftAssert();
         driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector("#login-button")).click();
-
-        //div[@class='inventory_item'][1]
         String nameProduct = driver.findElement(By.xpath("//div[@class='inventory_item'][1]//div[contains(@class,'item_name')]")).getText();
         String costProduct = driver.findElement(By.xpath("//div[@class='inventory_item'][1]//div[contains(@class,'item_price')]")).getText();
-
         driver.findElement(By.xpath("//div[@class='inventory_item'][1]//button")).click();
-
         driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
-        Assert.assertEquals(nameProduct, driver.findElement(By.xpath("//div[@class='cart_item'][1]//div[contains(@class,'item_name')]")).getText());
-        Assert.assertEquals(costProduct, driver.findElement(By.xpath("//div[@class='cart_item'][1]//div[@class='inventory_item_price']")).getText());
-
+        softAssert.assertEquals(nameProduct, driver.findElement(By.xpath("//div[@class='cart_item'][1]//div[contains(@class,'item_name')]")).getText());
+        softAssert.assertEquals(costProduct, driver.findElement(By.xpath("//div[@class='cart_item'][1]//div[@class='inventory_item_price']")).getText());
         driver.quit();
+        softAssert.assertAll();
     }
 }
